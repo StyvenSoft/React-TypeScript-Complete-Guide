580c1a9606b8e091f5ae191f795c6fff7881887e
Styvensoft <steveen.echeverri@gmail.com>
2025-02-05 21:24:04 -0500

    Added missing props

--- BasicReactjsProjects/guitar-shopping-cart/src/App.jsx
+++ BasicReactjsProjects/guitar-shopping-cart/src/App.jsx
@@ -24,6 +24,8 @@ function App() {
         increaseQuantity={increaseQuantity}
         decreaseQuantity={decreaseQuantity}
         clearCart={clearCart}
+        isEmpty={isEmpty}
+        cartTotal={cartTotal}
       />
 
       <main className="container-xl mt-5">
