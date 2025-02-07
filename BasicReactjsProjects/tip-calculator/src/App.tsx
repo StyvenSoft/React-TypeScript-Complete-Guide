16332324a8326bb3bf197e430c8f98e3008f4250
Styvensoft <steveen.echeverri@gmail.com>
2025-02-06 22:12:50 -0500

    Adding Main Menu Components

--- BasicReactjsProjects/tip-calculator/src/App.tsx
+++ BasicReactjsProjects/tip-calculator/src/App.tsx
@@ -1,9 +1,29 @@
+import MenuItem from "./components/MenuItem";
+import { menuItems } from "./data/db"
 
 function App() {
 
+  console.log(menuItems);
+  
   return (
     <>
-      <h1>Welcome reactJS</h1>
+      <header className="bg-sky-950 py-5">
+        <h1 className="text-center text-2xl text-white">Calculadora de Propinas y consumo</h1>
+      </header>
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menú</h2>
+          {menuItems.map(item => (
+            <MenuItem 
+              key={item.id}
+              item={item}
+            />
+          ))}
+        </div>
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
   )
 }
