45e55afdc8ba8bbdb17434624b80abc12b97d26b
Styvensoft <steveen.echeverri@gmail.com>
2025-02-07 19:58:35 -0500

    Update menu items styles

--- BasicReactjsProjects/tip-calculator/src/App.tsx
+++ BasicReactjsProjects/tip-calculator/src/App.tsx
@@ -4,21 +4,23 @@ import { menuItems } from "./data/db"
 function App() {
 
   console.log(menuItems);
-  
+
   return (
     <>
       <header className="bg-sky-950 py-5">
         <h1 className="text-center text-2xl text-white">Calculadora de Propinas y consumo</h1>
       </header>
       <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
-        <div>
-          <h2>Menú</h2>
-          {menuItems.map(item => (
-            <MenuItem 
-              key={item.id}
-              item={item}
-            />
-          ))}
+        <div className="p-5">
+          <h2 className="text-4xl font-black">Menú</h2>
+          <div className="space-y-3 mt-10">
+            {menuItems.map(item => (
+              <MenuItem
+                key={item.id}
+                item={item}
+              />
+            ))}
+          </div> 
         </div>
         <div>
           <h2>Consumo</h2>
--- BasicReactjsProjects/tip-calculator/src/components/MenuItem.tsx
+++ BasicReactjsProjects/tip-calculator/src/components/MenuItem.tsx
@@ -6,9 +6,11 @@ type MenuItemProps = {
 
 export default function MenuItem({item}: MenuItemProps) {
   return (
-    <>
+    <button
+      className="border-2 bg-sky-950 hover:bg-sky-800 text-white w-full p-3 flex justify-between"
+    >
       <p>{item.name}</p>
-      <p className="font-black">{item.price}</p>
-    </>
+      <p className="font-black">${item.price}</p> 
+    </button>
   )
 }
