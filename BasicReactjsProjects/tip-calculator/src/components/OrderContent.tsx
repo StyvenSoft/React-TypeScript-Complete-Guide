44a1e9c95d76cf4f570e653eeadd43ed1b9b073c
Styvensoft <steveen.echeverri@gmail.com>
2025-02-08 22:06:39 -0500

    Removing items from the order

--- BasicReactjsProjects/tip-calculator/src/components/OrderContent.tsx
+++ BasicReactjsProjects/tip-calculator/src/components/OrderContent.tsx
@@ -1,26 +1,27 @@
 import { formatCurrency } from "../helpers"
-import { OrderItem } from "../types"
+import { MenuItem, OrderItem } from "../types"
 
 type OrderContentsProps = {
-    order: OrderItem[]
+    order: OrderItem[],
+    removeItem: (id: MenuItem['id']) => void
 }
 
-export default function OrderContents({ order }: OrderContentsProps) {
+export default function OrderContents({ order, removeItem }: OrderContentsProps) {
 
     return (
         <div>
             <h2 className="font-black text-4xl">Consumo</h2>
-            <div className="space-y-3 mt-5">
+            <div className="space-y-3 mt-10">
                 {order.length === 0 ?
                     <p className="text-center">La order esta vacia</p>
                     : (
                         order.map(item => (
                             <div
                                 key={item.id}
-                                className="flex justify-between border-t border-gray-200 py-5"
+                                className="flex justify-between items-center border-t border-gray-200 py-5 last-of-type:border-b"
                             >
                                 <div>
-                                    <p key={item.id}>
+                                    <p className="text-lg">
                                         {item.name} - {formatCurrency( item.price )}
                                     </p>
                                     <p className="font-black">
@@ -29,6 +30,7 @@ export default function OrderContents({ order }: OrderContentsProps) {
                                 </div>
                                 <button
                                     className="bg-red-600 h-8 w-8 rounded-full text-white"
+                                    onClick={() => removeItem(item.id)}
                                 >
                                     X
                                 </button>
--- BasicReactjsProjects/tip-calculator/src/hooks/useOrdet.ts
+++ BasicReactjsProjects/tip-calculator/src/hooks/useOrdet.ts
@@ -22,8 +22,14 @@ export default function useOrder() {
         }
     }
 
+    const removeItem = (id: MenuItem['id']) => {
+        console.log('Eliminando', id);
+        setOrder(order.filter(item => item.id !== id))
+    }
+
     return {
         order,
-        addItem
+        addItem,
+        removeItem
     }
 }
