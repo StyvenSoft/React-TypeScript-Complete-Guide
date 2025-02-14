5f7f37a5d7cbd688f82a70b8578add76890b513d
Styvensoft <steveen.echeverri@gmail.com>
2025-02-13 21:44:10 -0500

    Form basic created

--- BasicReactjsProjects/calorie-tracker/src/App.tsx
+++ BasicReactjsProjects/calorie-tracker/src/App.tsx
@@ -1,8 +1,21 @@
+import Form from "./components/Form"
+
 function App() {
 
   return (
     <>
-      <h1>Contador de calorias</h1>
+      <header className="bg-gray-800 py-3">
+        <div className="max-w-4xl mx-auto flex justify-between">
+          <h1 className="text-center text-lg font-bold text-white uppercase">
+            Contador de calorias
+          </h1>
+        </div>
+      </header>
+      <section className="bg-gray-700 py-20 px-5">
+        <div className="max-w-4xl mx-auto">
+          <Form />
+        </div>
+      </section>
     </>
   )
 }
---
+++ BasicReactjsProjects/calorie-tracker/src/components/Form.tsx
@@ -0,0 +1,7 @@
+export default function Form() {
+  return (
+    <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+        <p>Forlulario</p>
+    </form>
+  )
+}
