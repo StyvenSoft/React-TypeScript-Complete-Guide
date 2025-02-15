05d311c2a23d55e7c95248ee8d252e4acffd94e2
Styvensoft <steveen.echeverri@gmail.com>
2025-02-14 21:50:55 -0500

    Created Form options categories

---
+++ BasicReactjsProjects/calorie-tracker/src/data/categories.ts
@@ -0,0 +1,8 @@
+import type { Category } from "../types"
+
+export const categories: Category[] = [
+    { id: 1, name: 'Comida' },
+    { id: 2, name: 'Ejercicio' },
+    { id: 3, name: 'Trabajo' },
+    { id: 4, name: 'Deportes' },
+]
