f9ddede320eba8f9e411ab43de0534029708959e
Styvensoft <steveen.echeverri@gmail.com>
2025-02-15 21:39:22 -0500

    Created user Test fetching

---
+++ BasicReactjsProjects/usersProjectTest/src/App.jsx
@@ -0,0 +1,23 @@
+import { useFetch } from './useFetch'
+import './App.css'
+
+function App() {
+  const { data, loading, error } = useFetch('https://jsonplaceholder.typicode.com/users')
+
+  return (
+    <div className='App'>
+      <h1>Data Feching</h1>
+      <div className='card'>
+        <ul>
+          {error && <li>Error: {error}</li>}
+          {loading && <li>Loading...</li>}
+          {data?.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default App
