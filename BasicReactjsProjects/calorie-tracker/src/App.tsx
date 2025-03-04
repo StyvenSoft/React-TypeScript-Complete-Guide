7108a368d11a7318cd18a2ceff0022ce4603e6bf
Styvensoft <steveen.echeverri@gmail.com>
2025-03-03 22:07:23 -0500

    Summary of calories consumed

--- BasicReactjsProjects/calorie-tracker/src/App.tsx
+++ BasicReactjsProjects/calorie-tracker/src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useReducer } from "react"
 import Form from "./components/Form"
 import { activityReducer, initialState } from "./reducers/activity-reducer"
 import { ActivityList } from "./components/ActivityList";
+import CalorieTracker from "./components/CalorieTracker";
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
     localStorage.setItem('activities', JSON.stringify(state.activities))
   }, [state.activities])
 
-  const canRestartApp = () => useMemo(() => state.activities.length, [state.activities])
+  const canRestartApp = () => useMemo(() => state.activities.length > 0, [state.activities])
 
 
   return (
@@ -24,6 +25,7 @@ function App() {
           <button
             className="bg-gray-700 hover:bg-gray-900 p-2 font-bold uppercase text-white cursor-pointer rounded-lg text-sm disabled:opacity-10"
             disabled={!canRestartApp()}
+            onClick={() => dispatch({type: 'restart-app'})}
           >
             Reiniciar
           </button>
@@ -42,6 +44,13 @@ function App() {
           activities={state.activities}
           dispatch={dispatch}
         />
+      </section>
+      <section className="bg-gray-700 py-10">
+        <div className="max-w-4xl mx-auto">
+          <CalorieTracker
+            activities={state.activities}
+          />
+        </div>
       </section>
     </>
   )
