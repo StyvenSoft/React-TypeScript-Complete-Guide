ada2d47cd3357857315886670d633b02f5695b61
Styvensoft <steveen.echeverri@gmail.com>
2025-03-21 17:14:02 -0500

    Basic badge reducer was created

--- BasicReactjsProjects/context-expense-control/src/components/BudgeForm.tsx
+++ BasicReactjsProjects/context-expense-control/src/components/BudgeForm.tsx
@@ -1,21 +1,37 @@
+import React, { useMemo, useState } from "react"
+
 export default function BudgeForm() {
-  return (
-    <form className="space-y-5">
-        <div className="flex flex-col space-y-5">
-            <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">
-                Definir presupuesto
-            </label>
-            <input 
-                id="budget"
-                type="number"
-                className="w-full bg-white border border-gray-200 p-2"
-                placeholder="Define tu presupuesto"
-                name="budget"
-            />
-        </div>
-        <input type="submit" value="Definir presupuesto"
-            className="bg-blue-500 hover:bg-blue-600 cursor-pointer w-full p-2 text-white font-black uppercase"
-        />
-    </form>
-  )
+
+    const [budget, setBudget] = useState(0)
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setBudget(e.target.valueAsNumber)
+    }
+    const isValid = useMemo(() => {
+        return isNaN(budget) || budget <= 0
+    }, [budget])
+
+    return (
+        <form className="space-y-5">
+            <div className="flex flex-col space-y-5">
+                <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">
+                    Definir presupuesto
+                </label>
+                <input
+                    id="budget"
+                    type="number"
+                    className="w-full bg-white border border-gray-200 p-2"
+                    placeholder="Define tu presupuesto"
+                    name="budget"
+                    value={budget}
+                    onChange={handleChange}
+                />
+            </div>
+            <input
+                type="submit"
+                value="Definir presupuesto"
+                className="bg-blue-500 hover:bg-blue-600 cursor-pointer w-full p-2 text-white font-black uppercase disabled:opacity-40"
+                disabled={isValid}
+            /> 
+        </form>
+    )
 }
