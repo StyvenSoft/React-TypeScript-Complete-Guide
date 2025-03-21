4850856e9d942b0e5493f3dc176c817570ee4771
Styvensoft <steveen.echeverri@gmail.com>
2025-03-21 14:37:24 -0500

    The first budget component was created from

--- BasicReactjsProjects/context-expense-control/src/App.tsx
+++ BasicReactjsProjects/context-expense-control/src/App.tsx
@@ -1,8 +1,17 @@
+import BudgeForm from "./components/BudgeForm"
+
 function App() {
 
   return (
     <>
-      <h1>Control de gatos</h1>
+      <header className="bg-blue-400 py-8 max-h-72">
+        <h1 className="uppercase text-center font-black text-3xl text-white">
+          App planificador de Gastos
+        </h1>
+      </header>
+      <div className="max-w-3xl mx-auto bg-white show-lg rounded-lg mt-10 p-10">
+        <BudgeForm />
+      </div>
     </>
   )
 }
---
+++ BasicReactjsProjects/context-expense-control/src/components/BudgeForm.tsx
@@ -0,0 +1,21 @@
+export default function BudgeForm() {
+  return (
+    <form className="space-y-5">
+        <div className="flex flex-col space-y-5">
+            <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">
+                Definir presupuesto
+            </label>
+            <input 
+                id="budget"
+                type="number"
+                className="w-full bg-white border border-gray-200 p-2"
+                placeholder="Define tu presupuesto"
+                name="budget"
+            />
+        </div>
+        <input type="submit" value="Definir presupuesto"
+            className="bg-blue-500 hover:bg-blue-600 cursor-pointer w-full p-2 text-white font-black uppercase"
+        />
+    </form>
+  )
+}
