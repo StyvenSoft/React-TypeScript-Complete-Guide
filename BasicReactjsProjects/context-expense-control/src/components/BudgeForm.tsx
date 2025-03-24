44eb00d126489442878f843617028a182fedcc05
Styvensoft <steveen.echeverri@gmail.com>
2025-03-24 14:02:23 -0500

    Reducer actions from hook in Context

--- BasicReactjsProjects/context-expense-control/src/components/BudgeForm.tsx
+++ BasicReactjsProjects/context-expense-control/src/components/BudgeForm.tsx
@@ -1,8 +1,10 @@
 import React, { useMemo, useState } from "react"
+import { useBudget } from "../hooks/useBudget"
 
 export default function BudgeForm() {
 
     const [budget, setBudget] = useState(0)
+    const { dispatch } = useBudget()
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setBudget(e.target.valueAsNumber)
     }
@@ -10,8 +12,15 @@ export default function BudgeForm() {
         return isNaN(budget) || budget <= 0
     }, [budget])
 
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log('Añidr presupuesto');
+        dispatch({type: 'add-budget', payload: {budget}})
+        
+    }
+
     return (
-        <form className="space-y-5">
+        <form className="space-y-5" onSubmit={handleSubmit}>
             <div className="flex flex-col space-y-5">
                 <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">
                     Definir presupuesto
