0705ac105b1c2f019501c333b5c63052ca16c4f8
Styvensoft <steveen.echeverri@gmail.com>
2025-03-26 18:02:49 -0500

    Expense form was created

---
+++ BasicReactjsProjects/context-expense-control/src/components/ExpenseForm.tsx
@@ -0,0 +1,56 @@
+import { categories } from "../data/categories";
+
+export default function ExpenseForm() {
+    return (
+        <form className="space-y-5">
+            <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-400 py-2">
+                Nuevo Gasto
+            </legend>
+            <div className="flex flex-col gap-2">
+                <label htmlFor="expenseName" className="text-xl">Nombre Gasto:</label>
+                <input 
+                    type="text"
+                    id="expenseName"
+                    placeholder="Añadir nombre del gasto"
+                    className="bg-slate-100 p-2"
+                    name="expenseName"
+                />
+            </div>
+
+            <div className="flex flex-col gap-2">
+                <label htmlFor="amount" className="text-xl">Cantidad:</label>
+                <input 
+                    type="number"
+                    id="amount"
+                    placeholder="Añadir la cantidad de gasto"
+                    className="bg-slate-100 p-2"
+                    name="amount"
+                />
+            </div>
+
+            <div className="flex flex-col gap-2">
+                <label htmlFor="category" className="text-xl">Categoría:</label>
+                <select 
+                    id="category"
+                    className="bg-slate-100 p-2"
+                    name="category"
+                >
+                    <option value="Seleccione">-- Seleccione --</option>
+                    {categories.map(category => (
+                        <option
+                            key={category.id}
+                            value={category.id}
+                        >
+                            {category.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+            <input 
+                type="submit"
+                className="bg-blue-500 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg"
+                value={'Registrar Gasto'}
+            />
+        </form>
+    )
+}
--- BasicReactjsProjects/context-expense-control/src/components/ExpenseModal.tsx
+++ BasicReactjsProjects/context-expense-control/src/components/ExpenseModal.tsx
@@ -1,30 +1,37 @@
 import { Fragment } from "react";
 import { Dialog, DialogPanel, Transition, TransitionChild } from "@headlessui/react";
 import { PlusCircleIcon } from "@heroicons/react/24/solid";
+import { useBudget } from "../hooks/useBudget";
+import ExpenseForm from "./ExpenseForm";
 
 export default function ExpenseModal() {
+
+    const { state, dispatch } = useBudget()
+
+
     return (
         <>
             <div className="fixed right-5 bottom-5 flex items-center justify-center">
                 <button
                     type="button"
+                    onClick={() => dispatch({ type: 'show-modal'})}
                 >
                     <PlusCircleIcon className="w-16 h-16 text-blue-600 rounded-full" />
                 </button>
             </div>
 
-            <Transition appear show={false} as={Fragment}>
-                <Dialog as="div" className="relative z-10" onClose={() => {}}>
+            <Transition appear show={state.modal} as={Fragment}>
+                <Dialog as="div" className="relative z-10" onClose={() => dispatch({type: 'close-modal'})}>
                     <TransitionChild
                         as={Fragment}
-                        enter="ease-out duration-300"
+                        enter="duration-300 ease-out"
                         enterFrom="opacity-0"
                         enterTo="opacity-100"
-                        leave="ease-in duration-200"
+                        leave="duration-200 ease-in"
                         leaveFrom="opacity-100"
                         leaveTo="opacity-0"
                     >
-                        <div className="fixed inset-0 bg-black bg-opacity-75" />
+                        <div className="fixed inset-0 bg-black/25" />
                     </TransitionChild>
 
                     <div className="fixed inset-0 overflow-y-auto">
@@ -39,7 +46,7 @@ export default function ExpenseModal() {
                                 leaveTo="opacity-0 scale-95"
                             >
                                 <DialogPanel className="w-full max-w-3xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-
+                                    <ExpenseForm />
                                 </DialogPanel>
                             </TransitionChild>
                         </div>
