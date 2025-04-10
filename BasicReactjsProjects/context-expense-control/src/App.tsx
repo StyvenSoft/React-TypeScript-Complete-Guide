c68e4ae4394e5784bb44987649ef8c3eb63584d7
Styvensoft <steveen.echeverri@gmail.com>
2025-04-10 18:16:24 -0500

    expenses are recorded in localstorage

--- BasicReactjsProjects/context-expense-control/src/App.tsx
+++ BasicReactjsProjects/context-expense-control/src/App.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react"
+import { useEffect, useMemo } from "react"
 import BudgeForm from "./components/BudgeForm"
 import { useBudget } from "./hooks/useBudget"
 import { BudgetTraker } from "./components/BudgetTraker"
@@ -10,6 +10,11 @@ function App() {
   const { state } = useBudget()
 
   const isValidBudget = useMemo(() => state.budget > 0, [state.budget])
+
+  useEffect(() => {
+    localStorage.setItem('budget', state.budget.toString())
+    localStorage.setItem('expenses', JSON.stringify(state.expenses))
+  }, [state])
 
   return (
     <>
