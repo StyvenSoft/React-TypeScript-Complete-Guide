3578dec445b8a7d4d55bfaac8e6583cace396ae4
Styvensoft <steveen.echeverri@gmail.com>
2025-04-17 22:07:11 -0500

    created status for patient list

--- patients-app/src/components/PatientsList.tsx
+++ patients-app/src/components/PatientsList.tsx
@@ -1,5 +1,18 @@
+import { usePatientStore } from "../store"
+
 export default function PatientsList() {
+
+  const patients = usePatientStore((state) => state.patients)
+
   return (
-    <div>PatientsList</div>
+    <div className="md:w-1/2 lg:3/5 md:h-screen overflow-y-scroll">
+      {patients.length ? (
+        <p>Si hay pacientes</p>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+        </>
+      )}
+    </div>
   )
 }
