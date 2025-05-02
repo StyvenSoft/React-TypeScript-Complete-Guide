86964c63d4a0ff1cc2a7091b691dc46ae0c8dad1
Styvensoft <steveen.echeverri@gmail.com>
2025-05-01 20:57:50 -0500

    Creating a hook to check the weather

--- weather-react-app/src/App.tsx
+++ weather-react-app/src/App.tsx
@@ -1,14 +1,19 @@
 import styles from "./App.module.css";
 import Form from "./components/Form/Form";
+import useWeather from "./hooks/useWeather";
 
 function App() {
+
+  const fetchWeather = useWeather()
 
   return (
     <>
       <h1 className={styles.title}>Weather app</h1>
 
       <div className={styles.container}>
-        <p><Form /></p>
+        <Form
+          fetchWeather={fetchWeather}
+        />
         <p>2</p>
       </div>
     </>
