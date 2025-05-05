cc5bffcdbcfc2adf6cae7995393c05e532fb4bf3
Styvensoft <steveen.echeverri@gmail.com>
2025-05-04 19:01:12 -0500

    API call to get the weather

--- weather-react-app/src/hooks/useWeather.ts
+++ weather-react-app/src/hooks/useWeather.ts
@@ -1,7 +1,20 @@
+import axios from "axios";
+import { SearchType } from "../types";
+
+
 export default function useWeather() {
 
-    const fetchWeather = () => {
-        console.log('Consultando');
+    const fetchWeather = async (search: SearchType) => {
+
+        const appId = 'b995e6755b8658ed5dc9cca227983a51'
+        try {
+            const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
+            const data = await axios(geoURL, { method: 'get' })
+            console.log(data);
+            
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     return (
