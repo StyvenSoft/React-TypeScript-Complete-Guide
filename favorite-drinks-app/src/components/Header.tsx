a0b7f400347f101cdf629c5c8173edc3a15aac05
Styvensoft <steveen.echeverri@gmail.com>
2025-05-16 18:50:10 -0500

    Basic header navigation was created

--- favorite-drinks-app/src/components/Header.tsx
+++ favorite-drinks-app/src/components/Header.tsx
@@ -1,11 +1,29 @@
+import { Link, NavLink } from "react-router-dom";
+
 export default function Header() {
     return (
         <header className="bg-slate-700">
-            <div className="mx-auto container px-5 py-16">
+            <div className="mx-auto container px-5 py-10">
                 <div className="flex justify-between items-center">
                     <div>
-                        <img className="w-32" src="/logo.svg" alt="Logotipo" />
+                        <Link to="/">
+                            <img className="w-32" src="/logo.svg" alt="Logotipo" />
+                        </Link>
                     </div>
+                    <nav className="flex gap-4">
+                        <NavLink to="/"
+                            className={({ isActive }) =>
+                                isActive ? 'text-orange-400 uppercase font-bold'
+                                    : 'text-white uppercase font-bold'}>
+                            Inicio
+                        </NavLink>
+                        <NavLink to="/favorites"
+                            className={({ isActive }) =>
+                                isActive ? 'text-orange-400 uppercase font-bold'
+                                    : 'text-white uppercase font-bold'}>
+                            Favoritos
+                        </NavLink>
+                    </nav>
                 </div>
             </div>
         </header>
