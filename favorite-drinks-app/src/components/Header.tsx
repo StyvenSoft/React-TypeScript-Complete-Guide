b7e0469d052f711b313cc19b62ec109d95e6a73c
Styvensoft <steveen.echeverri@gmail.com>
2025-05-19 22:10:10 -0500

    Creating initial search form

--- favorite-drinks-app/src/components/Header.tsx
+++ favorite-drinks-app/src/components/Header.tsx
@@ -1,6 +1,11 @@
-import { Link, NavLink } from "react-router-dom";
+import { useMemo } from "react";
+import { Link, NavLink, useLocation } from "react-router-dom";
 
 export default function Header() {
+
+    const { pathname } = useLocation()
+    const isHome = useMemo(() => pathname === '/', [pathname])
+    
     return (
         <header className="bg-slate-700">
             <div className="mx-auto container px-5 py-10">
@@ -25,6 +30,23 @@ export default function Header() {
                         </NavLink>
                     </nav>
                 </div>
+                { isHome && (
+                    <form className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-15 p-10 rounded-lg">
+                        <div className="space-y-4">
+                            <label 
+                                htmlFor="ingredient"
+                                className="block text-white uppercase font-extrabold text-lg"
+                            >Nombre o Ingredientes</label>
+                            <input 
+                                type="text"
+                                id="ingredients"
+                                name="ingredients"
+                                className="p-3 w-full rounded-lg focus:outline-none bg-white text-gray-900"
+                                placeholder="Nombre o ingrediente."
+                            />
+                        </div>
+                    </form>
+                )}
             </div>
         </header>
     )
