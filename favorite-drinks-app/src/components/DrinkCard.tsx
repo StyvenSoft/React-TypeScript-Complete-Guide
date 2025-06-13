31c6df72dc105f4e5cb258527eab68df38458c7b
Styvensoft <steveen.echeverri@gmail.com>
2025-06-12 21:33:23 -0500

    View of the drink cards

--- favorite-drinks-app/src/components/DrinkCard.tsx
+++ favorite-drinks-app/src/components/DrinkCard.tsx
@@ -7,10 +7,22 @@ type DrinkCardProps = {
 export default function DrinkCard({drink} : DrinkCardProps) {
     return (
         <div className="border shadow-lg">
-            <div>
-                <img src={drink.strDrinkThumb} alt={`Imagen de ${drink.strDrink}`} />
+            <div className="overflow-hidden">
+                <img 
+                    src={drink.strDrinkThumb} 
+                    alt={`Imagen de ${drink.strDrink}`}
+                    className="hover:scale-110 transition-transform hover:rotate-2"
+                />
             </div>
-            <h2>{drink.strDrink}</h2>
+            <div className="p-5">
+                <h2 className="text-2xl truncate font-black">{drink.strDrink}</h2>
+                <button
+                    type="button"
+                    className="cursor-pointer bg-orange-400 hover:bg-orange-500 mt-5 w-full p-3 font-bold text-white text-lg rounded-lg"
+                >
+                    Ver receta
+                </button>
+            </div>
         </div>
     )
 }
--- favorite-drinks-app/src/pages/indexPage.tsx
+++ favorite-drinks-app/src/pages/indexPage.tsx
@@ -9,14 +9,14 @@ export default function indexPage() {
     <>
       <h1 className="text-4xl font-extrabold">Recetas</h1>
       {hasDrinks ? (
-        <>
+        <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
           {drinks.drinks.map((drink) => (
             <DrinkCard
               key={drink.idDrink}
               drink={drink}
             />
           ))}
-        </>
+        </div>
       ) : (
         <p className="my-10 text-center text-2xl">No hay resultados</p>
       )}
